edfc55d46c30b59323905b52ef5a181518dac757
Y-Klein <k0556714698@gmail.com>
2025-11-26 12:30:13 +0200

    add models

---
+++ nudals/bed.py
@@ -0,0 +1,6 @@
+from  structure import Structure
+
+
+class Bed(Structure):
+    def __init__(self, jj):
+        pass
--- nudals/soldier.py
+++ nudals/soldier.py
@@ -1,1 +1,11 @@
+from basis import Basis
 
+
+class Soldier(Basis):
+    def __init__(self,personal_number,first_name,lest_name,gender,residential_city,distance_from_the_basis):
+        self.personal_number = personal_number
+        self.first_name = first_name
+        self.lest_name = lest_name
+        self.gender = gender
+        self.residential_city =residential_city
+        self.distance_from_the_basis =distance_from_the_basis
