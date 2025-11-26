501e210e5a339362e57cf1d860a793a51b2bfb5e
Y-Klein <k0556714698@gmail.com>
2025-11-26 12:00:20 +0200

    API up date baysik

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,UploadFile
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.post("/assignWithCsv")
+def upload_csv(csvFile:UploadFile):
+    return "file uploaded"
+
+
